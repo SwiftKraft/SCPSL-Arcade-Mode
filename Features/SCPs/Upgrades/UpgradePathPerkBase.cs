4016188f9a63e04553dc362629c97bfbcfc237be
SwiftKraft <shihuamian@gmail.com>
2025-06-18 13:33:45 -0400

    a

--- Features/SCPs/Upgrades/UpgradePathPerkBase.cs
+++ Features/SCPs/Upgrades/UpgradePathPerkBase.cs
@@ -1,14 +1,54 @@
-﻿using PlayerRoles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace SwiftUHC.Features.SCPs.Upgrades
 {
     public abstract class UpgradePathPerkBase(PerkInventory inv) : PerkBase(inv)
     {
-        
+        public readonly List<UpgradeBase> Path = [];
+
+        public int Progress { get => _progress; set => _progress = value; }
+        int _progress;
+
+        public override void Init()
+        {
+            base.Init();
+
+            Progress = 1;
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (Path.Count <= 0)
+                return;
+
+            for (int i = 0; i < Mathf.Min(Progress, Path.Count); i++)
+                Path[i].Tick();
+        }
+
+        public override void Remove()
+        {
+            base.Remove();
+
+            if (Path.Count <= 0)
+                return;
+
+            for (int i = 0; i < Mathf.Min(Progress, Path.Count); i++)
+                Path[i].Remove();
+        }
+    }
+
+    public abstract class UpgradeBase
+    {
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+
+        public virtual void Init() { }
+
+        public virtual void Tick() { }
+
+        public virtual void Remove() { }
     }
 }
