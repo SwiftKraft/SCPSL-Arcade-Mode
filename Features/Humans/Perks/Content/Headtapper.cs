294ac3ba76b567d69331b1340df218ce3b9b4d2c
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-06-22 14:39:20 -0400

    s

---
+++ Features/Humans/Perks/Content/Headtapper.cs
@@ -0,0 +1,41 @@
+﻿using LabApi.Events.Handlers;
+using PlayerStatsSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftUHC.Features.Humans.Perks.Content
+{
+    [Perk("Headtapper", Rarity.Uncommon)]
+    public class Headtapper(PerkInventory inv) : PerkBase(inv)
+    {
+        public override string Name => "Headtapper";
+
+        public override string Description => "Deal bonus damage when headshotting a human, \ntorso and limb shots damage reduced. \nSCP damage unchanged.";
+
+        public virtual float HeadMultiplier => 2f;
+        public virtual float BodyMultiplier => 0.5f;
+
+        public override void Init()
+        {
+            base.Init();
+            PlayerEvents.Hurting += OnPlayerHurting;
+        }
+
+        public override void Remove()
+        {
+            base.Remove();
+            PlayerEvents.Hurting -= OnPlayerHurting;
+        }
+
+        private void OnPlayerHurting(LabApi.Events.Arguments.PlayerEvents.PlayerHurtingEventArgs ev)
+        {
+            if (ev.Attacker != Player || ev.Player.IsSCP || ev.DamageHandler is not FirearmDamageHandler handler)
+                return;
+
+            handler.Damage *= handler.Hitbox == HitboxType.Headshot ? HeadMultiplier : BodyMultiplier;
+        }
+    }
+}
---
+++ Features/SCPs/Upgrades/Content/SCP939/Lurker/Lurker.cs
@@ -0,0 +1,18 @@
+﻿using PlayerRoles;
+using System;
+
+namespace SwiftUHC.Features.SCPs.Upgrades.Content.SCP939.Lurker
+{
+    [UpgradePath(RoleTypeId.Scp939)]
+    [Perk("939.Lurker", Rarity.Rare, PerkRestriction.SCP)]
+    public class Lurker(PerkInventory inv) : UpgradePathPerkBase(inv)
+    {
+        public override string PathName => "Lurker";
+
+        public override string PathDescription => "Surprise your enemies.";
+
+        public override Type[] AllUpgrades => [
+            typeof(CeilingStalker),
+            ];
+    }
+}
