c3108b9719cc1d7c012b0fa2fdb9d32c26e9c0ed
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-06-26 13:51:23 -0400

    a

---
+++ Commands/RA/DropPerkCommand.cs
@@ -0,0 +1,40 @@
+﻿using CommandSystem;
+using LabApi.Features.Wrappers;
+using SwiftUHC.Features;
+using SwiftUHC.Features.Humans.Perks;
+using System;
+
+namespace SwiftUHC.Commands.RA
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class DropPerkCommand : ICommand
+    {
+        public string Command => "dropperk";
+
+        public string[] Aliases => ["dperk", "dropp", "dp"];
+
+        public string Description => "Drops a perk.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!sender.CheckPermission(PlayerPermissions.Effects))
+            {
+                response = "No permission! ";
+                return false;
+            }
+
+            Player p = Player.Get(sender);
+
+            if (arguments.Array.Length < 2 || !PerkManager.TryGetPerk(arguments.Array[1].ToLower(), out PerkAttribute t))
+            {
+                response = "Unknown perk! ";
+                return false;
+            }
+
+            PerkSpawner.SpawnPerk(t, p.Position);
+
+            response = "Dropped perk: " + t.ID;
+            return true;
+        }
+    }
+}
