0f942b35e62a749dfcea41d195b108a597eb3921
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-09-24 19:39:44 -0400

    a

---
+++ Utils/Projectiles/ProjectileManager.cs
@@ -0,0 +1,9 @@
+﻿namespace SwiftArcadeMode.Utils.Projectiles
+{
+    public static class ProjectileManager
+    {
+        public static void Enable() { }
+        public static void Tick() { }
+        public static void Disable() { }
+    }
+}
