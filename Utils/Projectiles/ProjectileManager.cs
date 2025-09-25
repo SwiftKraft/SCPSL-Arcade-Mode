0421ae124867d479deeaee61a4ed58033680c942
SwiftKraft <shihuamian@gmail.com>
2025-09-25 14:04:06 -0400

    Update ProjectileManager.cs

--- Utils/Projectiles/ProjectileManager.cs
+++ Utils/Projectiles/ProjectileManager.cs
@@ -1,7 +1,11 @@
-﻿namespace SwiftArcadeMode.Utils.Projectiles
+﻿using System.Collections.Generic;
+
+namespace SwiftArcadeMode.Utils.Projectiles
 {
     public static class ProjectileManager
     {
+        public static readonly List<ProjectileBase> All = [];
+
         public static void Enable() { }
         public static void Tick() { }
         public static void Disable() { }
