937952ff793fa1993ccb1bb9141ec6bccf2c01c1
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-10-24 16:27:03 -0400

    a

--- Features/Humans/Perks/Content/Resupplier.cs
+++ Features/Humans/Perks/Content/Resupplier.cs
@@ -16,15 +16,54 @@ namespace SwiftArcadeMode.Features.Humans.Perks.Content
         {
             base.Init();
 
-            Player.SetAmmo(ItemType.Ammo12gauge, 1);
-            Player.SetAmmo(ItemType.Ammo9x19, 1);
-            Player.SetAmmo(ItemType.Ammo44cal, 1);
-            Player.SetAmmo(ItemType.Ammo556x45, 1);
-            Player.SetAmmo(ItemType.Ammo762x39, 1);
+            Player.SetAmmo(ItemType.Ammo12gauge, 999);
+            Player.SetAmmo(ItemType.Ammo9x19, 999);
+            Player.SetAmmo(ItemType.Ammo44cal, 999);
+            Player.SetAmmo(ItemType.Ammo556x45, 999);
+            Player.SetAmmo(ItemType.Ammo762x39, 999);
 
             PlayerEvents.ReloadingWeapon += OnReloadingWeapon;
             PlayerEvents.AimedWeapon += OnAimedWeapon;
             PlayerEvents.DroppingAmmo += OnDroppingAmmo;
+            PlayerEvents.Cuffing += OnCuffing;
+            PlayerEvents.Uncuffing += OnUncuffing;
+            PlayerEvents.Dying += OnDying;
+        }
+
+        private void OnDying(PlayerDyingEventArgs ev)
+        {
+            if (!ev.IsAllowed || ev.Player != Player)
+                return;
+
+            Player.SetAmmo(ItemType.Ammo12gauge, 0);
+            Player.SetAmmo(ItemType.Ammo9x19, 0);
+            Player.SetAmmo(ItemType.Ammo44cal, 0);
+            Player.SetAmmo(ItemType.Ammo556x45, 0);
+            Player.SetAmmo(ItemType.Ammo762x39, 0);
+        }
+
+        private void OnUncuffing(PlayerUncuffingEventArgs ev)
+        {
+            if (!ev.IsAllowed || ev.Target != Player)
+                return;
+
+            Player.SetAmmo(ItemType.Ammo12gauge, 999);
+            Player.SetAmmo(ItemType.Ammo9x19, 999);
+            Player.SetAmmo(ItemType.Ammo44cal, 999);
+            Player.SetAmmo(ItemType.Ammo556x45, 999);
+            Player.SetAmmo(ItemType.Ammo762x39, 999);
+        }
+
+        private void OnCuffing(PlayerCuffingEventArgs ev)
+        {
+            if (!ev.IsAllowed || ev.Target != Player)
+                return;
+
+            Player.SetAmmo(ItemType.Ammo12gauge, 0);
+            Player.SetAmmo(ItemType.Ammo9x19, 0);
+            Player.SetAmmo(ItemType.Ammo44cal, 0);
+            Player.SetAmmo(ItemType.Ammo556x45, 0);
+            Player.SetAmmo(ItemType.Ammo762x39, 0);
         }
 
         public override void Remove()
@@ -34,6 +73,9 @@ namespace SwiftArcadeMode.Features.Humans.Perks.Content
             PlayerEvents.ReloadingWeapon -= OnReloadingWeapon;
             PlayerEvents.AimedWeapon -= OnAimedWeapon;
             PlayerEvents.DroppingAmmo -= OnDroppingAmmo;
+            PlayerEvents.Cuffing -= OnCuffing;
+            PlayerEvents.Uncuffing -= OnUncuffing;
+            PlayerEvents.Dying -= OnDying;
         }
 
         private void OnAimedWeapon(PlayerAimedWeaponEventArgs ev)
@@ -41,11 +83,11 @@ namespace SwiftArcadeMode.Features.Humans.Perks.Content
             if (ev.Player != Player)
                 return;
 
-            Player.SetAmmo(ItemType.Ammo12gauge, 1);
-            Player.SetAmmo(ItemType.Ammo9x19, 1);
-            Player.SetAmmo(ItemType.Ammo44cal, 1);
-            Player.SetAmmo(ItemType.Ammo556x45, 1);
-            Player.SetAmmo(ItemType.Ammo762x39, 1);
+            Player.SetAmmo(ItemType.Ammo12gauge, 999);
+            Player.SetAmmo(ItemType.Ammo9x19, 999);
+            Player.SetAmmo(ItemType.Ammo44cal, 999);
+            Player.SetAmmo(ItemType.Ammo556x45, 999);
+            Player.SetAmmo(ItemType.Ammo762x39, 999);
         }
 
         private void OnDroppingAmmo(PlayerDroppingAmmoEventArgs ev)
@@ -59,7 +101,7 @@ namespace SwiftArcadeMode.Features.Humans.Perks.Content
             if (ev.Player != Player || Player.CurrentItem == null || Player.CurrentItem.Type == ItemType.GunSCP127 || Player.CurrentItem.Type == ItemType.ParticleDisruptor || Player.CurrentItem is not FirearmItem item || !item.Base.TryGetModule(out IPrimaryAmmoContainerModule mod) || mod.AmmoType == ItemType.None)
                 return;
 
-            Player.SetAmmo(mod.AmmoType, (ushort)(mod.AmmoMax - mod.AmmoStored + 1));
+            Player.SetAmmo(mod.AmmoType, 999);
         }
     }
 }
