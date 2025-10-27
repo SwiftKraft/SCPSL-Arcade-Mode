466de73988ac6b68ae02244bd884ceb75b681b98
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-10-26 21:40:43 -0400

    d

---
+++ Utils/Deployable/DeployableBase.cs
@@ -0,0 +1,9 @@
+﻿using ProjectMER.Features.Objects;
+
+namespace SwiftArcadeMode.Utils.Deployable
+{
+    public class DeployableBase
+    {
+        public SchematicObject Schematic { get; set; }
+    }
+}
