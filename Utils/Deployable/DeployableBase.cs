5f270fd04c9fc36503124489e9a5f9d38833ca69
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-11-27 09:52:19 -0500

    a

--- Utils/Deployable/DeployableBase.cs
+++ Utils/Deployable/DeployableBase.cs
@@ -13,17 +13,46 @@ namespace SwiftArcadeMode.Utils.Deployable
     {
         public string Name { get; private set; }
 
+        public virtual string TypeName => GetType().Name;
+
+        public Vector3 Position
+        {
+            get => position;
+            set
+            {
+                position = value;
+                Dummy.Position = position;
+                Schematic.Position = position;
+            }
+        }
+        private Vector3 position;
+
+        public Quaternion Rotation
+        {
+            get => rotation;
+            set
+            {
+                rotation = value;
+                Dummy.Rotation = rotation;
+                Schematic.Rotation = rotation;
+            }
+        }
+        private Quaternion rotation;
+
         public SchematicObject Schematic { get; set; }
 
         public Player Dummy { get; set; }
 
-        public DeployableBase(string name, string schematicName, RoleTypeId role, Vector3 colliderScale, Vector3 position)
+        public DeployableBase(string name, string schematicName, RoleTypeId role, Vector3 colliderScale, Vector3 position, Quaternion rotation)
         {
-            Dummy = Player.Get(DummyUtils.SpawnDummy(name));
+            Name = name;
+            Dummy = Player.Get(DummyUtils.SpawnDummy(Name));
+            Dummy.CustomInfo = TypeName;
             Dummy.Role = role;
             Dummy.Scale = colliderScale;
-            Dummy.Position = position;
-            Schematic = ObjectSpawner.SpawnSchematic(schematicName, position);
+            Schematic = ObjectSpawner.SpawnSchematic(schematicName, position, rotation);
+            Position = position;
+            Rotation = rotation;
             Dummy.EnableEffect<Fade>(byte.MaxValue);
             DeployableManager.AllDeployables.Add(this);
         }
